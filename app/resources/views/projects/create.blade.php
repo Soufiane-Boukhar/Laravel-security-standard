59e00ce72e65df29fa76e7b7530c1eb0891d2395
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2023-11-20 10:42:45 +0100

    update views

--- app/resources/views/projects/create.blade.php
+++ app/resources/views/projects/create.blade.php
@@ -1,46 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-    @if(auth()->check() && (auth()->user()->hasRole('admin') || auth()->user()->can('create projects')))
+@if(auth()->check() && (auth()->user()->hasRole('admin') || auth()->user()->can('create projects')))
 
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-12">
-                        <h1>Create Projects</h1>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <div class="content px-3">
-
-            @include('adminlte-templates::common.errors')
-
-            <div class="card">
-
-                {!! Form::open(['route' => 'projects.store']) !!}
-
-                <div class="card-body">
-
-                    <div class="row">
-                        @include('projects.fields')
-                    </div>
-
-                </div>
-
-                <div class="card-footer">
-                    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-                    <a href="{{ route('projects.index') }}" class="btn btn-default">Cancel</a>
-                </div>
-
-                {!! Form::close() !!}
-
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-12">
+                <h1>Create Projects</h1>
             </div>
         </div>
-    @else
-        <div class="container d-flex justify-content-center">
-            <h1>You don't have access</h1>
+    </div>
+</section>
+
+<div class="content px-3">
+
+    @include('adminlte-templates::common.errors')
+
+    <div class="card">
+
+        {!! Form::open(['route' => 'projects.store']) !!}
+
+        <div class="card-body">
+
+            <div class="row">
+                @include('projects.fields')
+            </div>
+
         </div>
-    @endif
+
+        <div class="card-footer">
+            {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+            <a href="{{ route('projects.index') }}" class="btn btn-default">Cancel</a>
+        </div>
+
+        {!! Form::close() !!}
+
+    </div>
+</div>
+@else
+<div class="container-wrapper">
+    <div class="div">
+        <div class="container">
+            <img src="{{ asset('assets/denied.png') }}" alt="" srcset="" width="700vw">
+        </div>
+        <div class="title">
+            <h3>You don't have access to this page</h3>
+        </div>
+    </div>
+</div>
+
+@endif
 @endsection
--- app/resources/views/projects/edit.blade.php
+++ app/resources/views/projects/edit.blade.php
@@ -4,40 +4,52 @@
 
 @if (auth()->check() && (auth()->user()->hasRole('admin') || auth()->user()->can('edit projects')))
 
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Edit Project
-                    </h1>
-                </div>
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-12">
+                <h1>
+                    Edit Project
+                </h1>
             </div>
-        </div>
-    </section>
-
-    <div class="content px-3">
-
-        @include('adminlte-templates::common.errors')
-
-        <div class="card">
-
-            {!! Form::model($project, ['route' => ['projects.update', $project->id], 'method' => 'patch']) !!}
-
-            <div class="card-body">
-                <div class="row">
-                    @include('projects.fields')
-                </div>
-            </div>
-
-            <div class="card-footer">
-                {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-                <a href="{{ route('projects.index') }}" class="btn btn-default"> Cancel </a>
-            </div>
-
-            {!! Form::close() !!}
-
         </div>
     </div>
+</section>
+
+<div class="content px-3">
+
+    @include('adminlte-templates::common.errors')
+
+    <div class="card">
+
+        {!! Form::model($project, ['route' => ['projects.update', $project->id], 'method' => 'patch']) !!}
+
+        <div class="card-body">
+            <div class="row">
+                @include('projects.fields')
+            </div>
+        </div>
+
+        <div class="card-footer">
+            {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+            <a href="{{ route('projects.index') }}" class="btn btn-default"> Cancel </a>
+        </div>
+
+        {!! Form::close() !!}
+
+    </div>
+</div>
+@else
+<div class="container-wrapper">
+    <div class="div">
+        <div class="container">
+            <img src="{{ asset('assets/denied.png') }}" alt="" srcset="" width="700vw">
+        </div>
+        <div class="title">
+            <h3>You don't have access to this page</h3>
+        </div>
+    </div>
+</div>
+
 @endif
 @endsection
